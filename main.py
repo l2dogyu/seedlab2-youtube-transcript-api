da8cb9a833dd88d66008a2c8ab344b34a91e35fa
l2dogyu <l2.dogyu@gmail.com>
2025-05-04 19:49:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 
 app = FastAPI()
 
+PROXY = {
+    "http": "http://133.18.234.13:80",  # 여기에 프록시 주소를 넣으세요
+    "https": "http://133.18.234.13:80"
+}
+
 @app.get("/transcript")
 def get_transcript(videoId: str = Query(..., description="YouTube video ID")):
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(videoId, languages=['ko', 'en'])
+        transcript = YouTubeTranscriptApi.get_transcript(videoId, languages=['ko', 'en'], proxies=PROXY)
         full_text = ' '.join([entry['text'] for entry in transcript])
         return {"transcript": full_text}
     except TranscriptsDisabled:
